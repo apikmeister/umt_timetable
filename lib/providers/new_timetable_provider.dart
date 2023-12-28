e2ba1630adcbf32692c8cc387ecaa45d266693a8
apikmeister <afiq.mohamad90@gmail.com>
2023-12-29 02:07:57 +0800

    Add selectedYear and unselectedGroups properties to NewTimetableProvider

--- lib/providers/new_timetable_provider.dart
+++ lib/providers/new_timetable_provider.dart
@@ -4,9 +4,23 @@ class NewTimetableProvider extends ChangeNotifier {
   // String selectedSemester = '';
   String? _selectedSession;
   String? _selectedProgram;
+  String? _selectedYear;
+  Map<String, String?>? unselectedGroups;
 
   String? get selectedSession => _selectedSession;
   String? get selectedProgram => _selectedProgram;
+  String? get selectedYear => _selectedYear;
+  Map<String, String?>? get unselectedGroup => unselectedGroups;
+
+  void setUnselectedGroup(Map<String, String?> unselectedGroup) {
+    unselectedGroups = unselectedGroup;
+    notifyListeners();
+  }
+
+  void setSelectedYear(String year) {
+    _selectedYear = year;
+    notifyListeners();
+  }
 
   void setSelectedProgram(String program) {
     _selectedProgram = program;
