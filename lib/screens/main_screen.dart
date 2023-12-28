f9ae20519441097e35a4f2db4ad4be1b765a3193
apikmeister <afiq.mohamad90@gmail.com>
2023-12-29 02:09:34 +0800

    Remove unused imports and commented code, update UI layout

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_timetable_view/flutter_timetable_view.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,39 +23,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text(
-      //     'Timetable',
-      //     style: TextStyle(
-      //       color: Colors.black,
-      //       fontWeight: FontWeight.bold,
-      //       letterSpacing: 1.3,
-      //     ),
-      //   ),
-      //   backgroundColor: Colors.transparent,
-      //   elevation: 0,
-      //   // leading: IconButton(
-      //   //   onPressed: () {
-      //   //     Navigator.pop(context);
-      //   //   },
-      //   //   icon: const Icon(
-      //   //     Icons.arrow_back,
-      //   //     color: Colors.black,
-      //   //   ),
-      //   // ),
-      //   actions: <Widget>[
-      //     IconButton(
-      //       onPressed: () {},
-      //       icon: const RotatedBox(
-      //         quarterTurns: 1,
-      //         child: Icon(
-      //           Icons.tune,
-      //           color: Colors.black,
-      //         ),
-      //       ),
-      //     ),
-      //   ],
-      // ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15.0),
@@ -65,13 +32,14 @@ class _HomeScreenState extends State<HomeScreen> {
               Text(
                 'Welcome,\nGet Started!',
                 style: GoogleFonts.inter(
-                  // textStyle: Theme.of(context).textTheme.displayLarge,
                   fontSize: 45,
                   fontWeight: FontWeight.w900,
                   fontStyle: FontStyle.italic,
                 ),
               ),
-              // const SizedBox(height: 20),
+              const Divider(
+                thickness: 1,
+              ),
               Expanded(
                 child: hasTimetable ? _buildTimetable() : _buildNoTimetable(),
               ),
@@ -79,14 +47,6 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     setState(() {
-      //       hasTimetable = !hasTimetable;
-      //     });
-      //   },
-      //   child: const Icon(Icons.add),
-      // ),
     );
   }
 
@@ -98,16 +58,16 @@ class _HomeScreenState extends State<HomeScreen> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Timetable'),
+            const Text('Timetable'),
             IconButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/create_timetable');
               },
-              icon: Icon(Icons.add, size: 20),
+              icon: const Icon(Icons.add, size: 20),
             ),
           ],
         ),
-        Spacer(),
+        const Spacer(),
         Center(
           child: ListView.builder(
             itemCount: timetableList.length,
@@ -118,56 +78,46 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           ),
         ),
-        Spacer(),
+        const Spacer(),
       ],
     );
-    // return Container(
-    //   child: ListView.builder(
-    //     itemCount: timetableList.length,
-    //     itemBuilder: (context, index) {
-    //       return ListTile(
-    //         title: Text(timetableList[index]),
-    //       );
-    //     },
-    //   ),
-    // );
-    // return ListView.builder(
-    //   itemCount: timetableEvents.length,
-    //   itemBuilder: (context, index) {
-    //     return ListTile(
-    //       title:
-    //           Text(timetableEvents[index].name), // Replace with actual property
-    //     );
-    //   },
-    // );
   }
 
   Widget _buildNoTimetable() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Create New Timetable'),
+            const Text('Create New Timetable'),
             IconButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/create_timetable');
               },
-              icon: Icon(Icons.add, size: 20),
+              icon: const Icon(Icons.add, size: 20),
             ),
           ],
         ),
-        Spacer(),
+        // Spacer(),
         Center(
           child: Column(
             children: [
-              Text('No timetable found'),
+              Lottie.asset(
+                'assets/lottie/notFoundAnim.json',
+                animate: false,
+              ),
+              Text(
+                'No timetable found',
+                style: GoogleFonts.inter(
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey,
+                ),
+              ),
             ],
           ),
         ),
-        Spacer(),
+        const Spacer(),
       ],
     );
   }
